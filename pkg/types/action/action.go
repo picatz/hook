06be14b2ba7d81da62d74d4ac2b1353a8d2dccb8
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2020-12-08 19:35:30 -0500

    Start defining more action types

--- pkg/types/action/action.go
+++ pkg/types/action/action.go
@@ -2,7 +2,25 @@ package action
 
 type Type uint32
 
+type (
+	General      = Type
+	HeaderAction = Type
+	DataAction   = Type
+)
+
 const (
-	Continue Type = 0
-	Pause    Type = 1
+	Continue General = 0
+	Pause    General = 1
+)
+
+const (
+	ContinueAndEndStream    HeaderAction = 2
+	PauseAndBufferHeader    HeaderAction = 3
+	PauseAndWatermarkHeader HeaderAction = 4
+)
+
+const (
+	StopIterationAndBufferData       DataAction = 1
+	StopIterationAndWatermarkData    DataAction = 2
+	StopAllIterationAndWatermarkData DataAction = 4
 )
